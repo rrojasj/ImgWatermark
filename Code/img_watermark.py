ad382d634dbdfb930dab4c145d44b6e42c982590
Rob <rrojasj@ucenfotec.ac.cr>
2023-02-14 20:20:02 -0600

    Pruebas para iniciar Git en el proyecto

--- Code/img_watermark.py
+++ Code/img_watermark.py
@@ -2,19 +2,27 @@
 # It provides an easy way for the user to interact with several os functions
 import os
 
+# importing Pillow library
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 """
 :function: read_files()
 :\
 """
+
 def read_files():
-    ruta_archivos = input("Ingrese la ruta del directorio de las imágenes \n")
+    file_path = input("Ingrese la ruta del directorio de las imágenes \n")
     ## Windows: C:/ImgWatermark/Documents
     ## Mac: /Users/robjimn/Documents/Roberto Rojas/Cenfotec
     print("Los archivos encontrados en la ruta indicada son: ")
 
-    for imagen in os.listdir(ruta_archivos):
-        if os.path.isfile(os.path.join(ruta_archivos, imagen)) and imagen.endswith('.png'):
-            print('- '+imagen)
+                                             #0          1           2
+    for image in os.listdir(file_path): #   [dir, "testing.png", "test.txt"]
+        if os.path.isfile(os.path.join(file_path, image)) and image.endswith('.png'):
+            print('- '+image)
     print('\n')
 
 # ============================================ #
@@ -26,22 +34,23 @@ def menu():
     print("[2] Agregar marca de agua a un archivo")
     print("[0] Salir del programa \n")
 
-opcion_menu = int(input("Seleccione una opción: \n"))
+option_menu = int(input("Seleccione una opción: \n"))
 
-while opcion_menu != 0:
-    if opcion_menu == 1:
+while option_menu != 0:
+    if option_menu == 1:
         # Obtener la lista de archivos del directorio
         print("Opción 1 ha sido seleccionada.\n")
         read_files()
        
-    elif opcion_menu == 2:
+    elif option_menu == 2:
         # Agregar marca de agua a un archivo
         print("Opción 2 ha sido seleccionada.\n")
+
     else:
         print("Opción inválida.\n")
 
     menu()
-    opcion_menu = int(input("Seleccione una opción: \n"))
+    option_menu = int(input("Seleccione una opción: \n"))
 
 print("Gracias por utilizar ImageWatermark Tool.\n")
 
---
+++ Code/test.py
@@ -0,0 +1,24 @@
+# --------------- COMMANDS TO INSTALL LIBRARIES ---------------- #
+
+#:numpy      - pip install numpy || pip3 install numpy
+#:details    - NumPy is a Python library used for working with arrays
+
+# -----  -----  -----  -----  -----  -----  -----  -----  -----  #
+
+#:matplotlib - pip install matplotlib
+#:details    - Matplotlib is a Python library used for creating static, animated, and interactive visualizations
+
+# -----  -----  -----  -----  -----  -----  -----  -----  -----  #
+
+# Codigo que ayuda a obtener la imagen...
+# from PIL import Image
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# image = Image.open("C:\ImgWatermark\Documents\/video_game_1.png")
+
+# image.show()
+# plt.imshow(image)
+
+# *********************   *********************   ********************* #
+# *********************   *********************   ********************* #
